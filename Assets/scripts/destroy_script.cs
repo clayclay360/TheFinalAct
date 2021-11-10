56b0b8435933724a9efdfb58eddcfbd85b1ab784
Clay <38699310+clayclay360@users.noreply.github.com>
2021-11-10 18:23:52 -0500

    Commented code

--- Assets/scripts/destroy_script.cs
+++ Assets/scripts/destroy_script.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class destroy_script : MonoBehaviour
 {
+    //variable
     public float timer;
-
+    
+    //destroy gameobject in certain amount of time
     void Start()
     {
         Destroy(gameObject, timer);
