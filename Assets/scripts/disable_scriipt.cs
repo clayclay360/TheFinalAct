d326c32ccd6dca575292cb1478f2a2ec9aa1a02a
Clay <38699310+clayclay360@users.noreply.github.com>
2021-11-10 18:24:53 -0500

    Commented code

--- Assets/scripts/disable_scriipt.cs
+++ Assets/scripts/disable_scriipt.cs
@@ -4,13 +4,16 @@ using UnityEngine;
 
 public class disable_scriipt : MonoBehaviour
 {
+    //variable
     public GameObject target_object;
 
+    //disables gameobject
     public void disable()
     {
         target_object.SetActive(false); // set game object inactive
     }
-
+    
+    //enable gameobject
     public void enable()
     {
         target_object.SetActive(true); // set game object active
