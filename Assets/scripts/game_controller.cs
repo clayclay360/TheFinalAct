2bc87ddb2f27ba4ebda33006bb4191006471c96a
Clay <38699310+clayclay360@users.noreply.github.com>
2021-11-10 18:42:37 -0500

    Commented code

--- Assets/scripts/game_controller.cs
+++ Assets/scripts/game_controller.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -6,6 +6,7 @@ using UnityEngine.SceneManagement;
 
 public class game_controller : MonoBehaviour
 {
+    //variables
     public bool game_started;
     public Text end_credit;
     public Text elmination_text;
@@ -18,7 +19,8 @@ public class game_controller : MonoBehaviour
     public Texture2D mouse_texture;
 
     private int elminations = 0;
-
+    
+    //get components, wait then call hide play button method
     public void start_game()
     {
         game_started = true;
@@ -26,28 +28,27 @@ public class game_controller : MonoBehaviour
         Invoke("hide_play_button", .75f);
     }
 
-    public void Update()
-    {
-        //get_mouse();
-    }
-
+    //hide the play button
     public void hide_play_button()
     {
         play_button.gameObject.SetActive(false);
     }
 
+    //set active the display credit, wait then call lower curtains method
     public void display_end_credit()
     {
         end_credit.gameObject.SetActive(true);
         Invoke("lower_curtains",1);
     }
-
+    
+    //trigger decsend animation and start restart game coroutine
     private void lower_curtains()
     {
         curtain_animator.SetTrigger("decsend");
         StartCoroutine(restart_game());
     }
-
+    
+    //display white screen then fade out to load new scene
     public IEnumerator restart_game()
     {
         yield return new WaitForSeconds(2);
@@ -55,7 +56,8 @@ public class game_controller : MonoBehaviour
         yield return new WaitForSeconds(1);
         SceneManager.LoadScene(0,LoadSceneMode.Single);
     }
-
+    
+    //find and destory all the bunnies
     public void destroy_all_bunnies()
     {
         bunny_controller[] bunnies = FindObjectsOfType<bunny_controller>();
@@ -66,6 +68,7 @@ public class game_controller : MonoBehaviour
         }
     }
 
+    //find and destroy all the cards
     public void destory_all_cards()
     {
         card_script[] cards = FindObjectsOfType<card_script>();
@@ -76,14 +79,10 @@ public class game_controller : MonoBehaviour
         }
     }
 
+    //add a point to elminations and display it in text
     public void add_point()
     {
         elminations += 1;
         elmination_text.text = elminations.ToString();
-    }
-
-    private void get_mouse()
-    {
-        Cursor.SetCursor(mouse_texture, new Vector2(16, 16), CursorMode.Auto);
     }
 }
